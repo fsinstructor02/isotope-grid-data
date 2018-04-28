31985de884e2104b826596b0edf8d5e276330f40
Joseph Williams <fsinstructor02@hussiancollege.edu>
2018-04-28 14:12:12 -0400

    complete

---
+++ js/data.js
@@ -0,0 +1,28 @@
+var dataC = [{  
+    "image": "red",
+    "title": "title 1"
+},{ 
+    "image": "blue",
+    "title": "title 2"
+},{ 
+    "image": "black",
+    "title": "title 3"
+},{ 
+    "image": "grey",
+    "title": "title 4"
+},{ 
+    "image": "yellow",
+    "title": "title 5"
+},{ 
+    "image": "lightblue",
+    "title": "title 6"
+},{ 
+    "image": "black",
+    "title": "title 7"
+},{ 
+    "image": "blue",
+    "title": "title 8"
+},{ 
+    "image": "red",
+    "title": "title 9"
+}]
--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 /*Js Jq functions etc*/
 jQuery(function(){ // JQuery function call to run code
 
+    var i;
+    var imageColor;
+    var titleName;
+
+    for (i = 0; i < dataC.length; i++) { 
+
+      imageColor =dataC[i]["image"];
+      titleName = dataC[i]["title"];
+
+      $(".grid-container").append( "<div class='grid-blocks'><div class='grid-blocks-image' style='background-color:"+ imageColor +"'></div><div class='grid-blocks-title'>" + titleName + "</div></div>" );
+    }
 
 
+      var $grid = $('.grid-container').isotope({
+        // main isotope options
+        itemSelector: '.grid-blocks',
+        // set layoutMode
+        layoutMode: 'masonry'
+      })
 
 });(jQuery); 
 
